f3481e6d1373324bd0e73d809b27d0c6ac4e00dd
Dor Shtaif <dorshtaif@gmail.com>
2024-12-26 10:24:05 +0200

    refactor: fix import path of `iterateFormatted` on package's public export (#17)

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { useAsyncIter, type IterationResult } from './useAsyncIter/index.js';
 import { Iterate, type IterateProps } from './Iterate/index.js';
-import { iterateFormatted, type FixedRefFormattedIterable } from './iterateFormatted//index.js';
+import { iterateFormatted, type FixedRefFormattedIterable } from './iterateFormatted/index.js';
 import { useAsyncIterState, type AsyncIterStateResult } from './useAsyncIterState/index.js';
 
 export {
